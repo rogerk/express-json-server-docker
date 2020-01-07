6b535fef90d3bcf983f2e8a8c22bf64fb0ed407e
rogerk <rkitain@yahoo.com>
2020-01-07 10:54:22 -0500

    - simple express server that GETs a json file contents.

---
+++ src/app.js
@@ -0,0 +1,23 @@
+import express from "express";
+import axios from "axios";
+import dotenv from "dotenv";
+
+dotenv.config();
+const port = process.env.PORT || 4001;
+const dbPort = process.env.DB_PORT;
+const dbHost = process.env.DB_HOST;
+
+const app = express();
+
+app.get("/matrix", async (req, res) => {
+    try {
+        const resp = await axios.get(`http://${dbHost}:${dbPort}/matrix`);
+        res.json(resp.data);
+    } catch (error) {
+        console.error(`Error: ${error}`);
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+});
